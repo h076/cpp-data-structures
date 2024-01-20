07f28354a1dbcdc2d86af3da4fd0405d2d0bcd36
h076 <hazawood@me.com>
2024-01-20 14:30:23 +0000

    inclusion model used due to templates

--- genericLinkedList/LinkedList.cpp
+++ genericLinkedList/LinkedList.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+// Interface
+
 template <typename T>
 class Node {
    public:
@@ -27,6 +29,8 @@ class LinkedList {
         int length();
         void printList();
 };
+
+// Implementation
 
 template <typename T>
 Node<T>::Node() {
@@ -190,39 +194,4 @@ void LinkedList<T>::printList() {
 
     std::cout << temp->data << "\n";
     return;
-}
-
-int main() {
-    LinkedList<int> intList;
-    LinkedList<char> charList;
-
-    intList.printList();
-
-    intList.insertNodeAt(77, 0);
-    charList.insertNodeAt('h', 0);
-    charList.insertNodeAt('a', 1);
-    charList.insertNodeAt('r', 2);
-    charList.insertNodeAt('r', 3);
-    charList.insertNodeAt('y', 4);
-
-    charList.printList();
-    intList.printList();
-
-    for(int i=0; i<20; i++) {
-        intList.insertNodeAt(i, 0);
-    }
-
-    intList.printList();
-
-    intList.deleteAt(76);
-
-    std::cout << "int list length is " << intList.length() << "\n";
-
-    intList.deleteAt(5);
-    charList.deleteAt(3);
-
-    std::cout << "int list length is " << charList.length() << "\n";
-
-    intList.printList();
-    charList.printList();
 }
